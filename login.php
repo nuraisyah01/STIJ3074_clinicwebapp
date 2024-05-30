c3685a96545f45143a78aaabf95d424d9293f34b
286752 Siti Nur Aisyah <92418770+nuraisyah01@users.noreply.github.com>
2024-05-30 20:46:25 +0800

    Update login.php

--- login.php
+++ login.php
@@ -51,7 +51,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -60,30 +59,74 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="w3.css">
     <title>Clinic Web App</title>
     <script src="scripts.js"></script>
+    <style>  
+
+    .grid-container {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 1px;
+        height:90vh;
+        background-image: url('img4.png');
+        background-size: cover;
+        background-position: center;
+        
+    }
+
+    .grid-left {
+        grid-column: 1;
+    }
+
+    .grid-right {
+        grid-column: 2;
+        padding: 20px;
+    }
+
+    @media only screen and (max-width: 768px) {
+       .grid-container {
+            grid-template-columns: 1fr;
+
+            
+        }
+       .grid-left {
+            display: none;
+        }
+       .grid-right {
+            grid-column: 1;
+            padding: 20px;
+            width: 100%;
+        }
+    }
+
+    
+</style>
 
 </head>
 <body onload="loadCookies()">
     <header class="w3-container w3-center w3-dark-grey w3-padding-32">
-        <h1>Clinic Web Application</h1>
-        <p>Clinic Login</p>
+        <h1>Clinic Web</h1>
+        <p class=w3-xlarge>Clinic Login</p>
     </header>
     <div class="w3-bar w3-dark-grey">
         <a href="index.html" class="w3-bar-item w3-button">Home</a>
     </div>
-    <div style="min-height:100vh;overflow-y: auto;">
-        <div class="w3-container w3-padding-64">
+    <div class="grid-container">
+        <div class="grid-left"></div>
+        <div class="grid-right">
             <div class="w3-card w3-round" style="max-width:600px;margin:auto">
-                <div class="w3-container w3-dark-grey">
+                <div class="w3-container w3-dark-grey w3-margin-top">
                     <h4>Login Form</h4>
                 </div>
-                <form name="loginForm" class="w3-container" action="login.php" method="post">
+                <form name="loginForm" class="w3-container w3-white " action="login.php" method="post">
+                <div class="imgcontainer" style="text-align: center;">
+                    <img src="img2.png" alt="Avatar" class="avatar w3-margin-top" style="width: 150px; height: 150px; border-radius: 50%; ">
+                </div>
                     <p>
                         <label><b>Email</b></label>
-                        <input class="w3-input w3-border w3-round" name="username" type="text" id="username"required>
+                        <input class="w3-input w3-border w3-round" name="username" placeholder="Enter email" type="text" id="username"required>
                     </p>
                     <p>
                         <label><b>Password</b></label>
-                        <input class="w3-input w3-border w3-round" name="password" type="password" id="password" required>
+                        <input class="w3-input w3-border w3-round" name="password" placeholder="Enter password" type="password" id="password" required>
                     </p>
                     
                     <p>
@@ -94,14 +137,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <button class="w3-btn w3-round w3-blue w3-block" name="submit" value="submit">Login</button>
                     </p>
                 </form>
-                <div class="w3-container w3-center">
+                <div class="w3-container w3-center w3-white w3-margin-bottom">
                     <p>Don't have an account? <a href="register.php" style="color:blue;">Register</a> here</p>
                 </div>
             </div>
         </div>
     </div>
     <footer class="w3-container w3-center w3-dark-grey">
-        <p>&copy; 2024 Clinic Web App. All rights reserved.</p>
+        <p>&copy; 2024 Clinic Web. All rights reserved.</p>
     </footer>
 </body>
 </html>
+
